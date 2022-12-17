bbf6ee92438f2c073360f223700c01b029ced6b5
Igor Grab <100761728+grabigor1990@users.noreply.github.com>
2022-12-17 11:08:00 +0100

    Update StubServise.java

--- src/main/com/telran/bank/service/StubServise.java
+++ src/main/com/telran/bank/service/StubServise.java
@@ -1,1 +1,24 @@
 
+package com.telran.bank.service;
+
+import com.telran.bank.repository.StubRepository;
+
+public class StubService {
+    private StubRepository stubRepository;
+
+
+    public double getBalance(int accountID){
+        return stubRepository.findBalanceByAccID(accountID);
+    }
+    public void depositAmount(int accountId,double amount){
+        stubRepository.saveBalanceByAccID(accountId,amount);
+    }
+    public void withdrawAmount(int accountID,double amount){
+        stubRepository.withdrawAmountByAccID(accountID,amount);
+    }
+    public void teansferAmount(int accountID,int destAcctID,double amount){
+        stubRepository.withdrawAmountByAcctID(accountID, amount);
+        stubRepository.saveBalanceByAcctID(destAcctID, amount);
+    }
+
+}
